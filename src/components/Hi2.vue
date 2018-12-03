74ab9bfac42d26fe15e94598672d0df78967ee2c
yangfeng <Colin520vip@163.com>
2018-12-03 19:56:38 +0800

    vue-cli脚手架

---
+++ src/components/Hi2.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+      <h2>{{ msg }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'hi2',
+  data () {
+    return {
+      msg: 'woshi Hi222 yem'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
